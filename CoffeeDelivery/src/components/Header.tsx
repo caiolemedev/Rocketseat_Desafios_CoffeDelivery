c38335317db07363953bbbcbfb34bf367f7e9f49
Caio Leme <caioacleme@gmail.com>
2023-06-06 20:07:42 -0300

    Added logo and header navigation
    Added logo and header navigation

--- CoffeeDelivery/src/components/Header.tsx
+++ CoffeeDelivery/src/components/Header.tsx
@@ -1,12 +1,22 @@
 import { MapPin } from '@phosphor-icons/react'
 
+import logo from '../assets/logo.png'
+import { NavLink } from 'react-router-dom'
+import { Cart } from './Cart'
+
 export function Header() {
   return (
     <div>
-      <a>Coffee Delivery</a>
-      <span>
-        <MapPin size={24} weight='fill' />
-      </span>
+      <NavLink to="/" title='logo'>
+        <img src={logo} alt="logo" />
+      </NavLink>
+      
+      <MapPin size={24} weight='fill' />
+      <span>Location</span>
+      
+      <NavLink to="/Checkout" title="checkout">
+        <Cart />
+      </NavLink>
     </div>
   )
 }
