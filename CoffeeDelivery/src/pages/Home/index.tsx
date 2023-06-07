30649fbd3ed3c894c0ee04f53f9fd48d0bf481cd
Caio Leme <caioacleme@gmail.com>
2023-06-07 16:36:01 -0300

    added content to home page
    added content to home page

--- CoffeeDelivery/src/pages/Home/index.tsx
+++ CoffeeDelivery/src/pages/Home/index.tsx
@@ -1,6 +1,20 @@
+import { ShoppingCart, Package, Timer, Coffee } from "@phosphor-icons/react"
 
 export function Home() {
   return (
-    <h1>Home</h1>
+    <div>
+      <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+      <p>Com o Coffee Delivery você rebece seu café onde estiver a qualquer hora</p>
+      <div>
+        <ShoppingCart weight="fill" />
+        <span>Compra simples e segura</span>
+        <Package weight="fill" />
+        <span>Embalagem mantém o café intacto</span>
+        <Timer weight="fill" />
+        <span>Entrega rápida e rastreada</span>
+        <Coffee weight="fill" />
+        <span>O café chega fresquinho até você</span>
+      </div>
+    </div>
   )
 }
